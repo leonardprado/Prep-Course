416f67d140ebda1d2371ec2df5f186d878ab35d4
Leonardo Prado <estudioprado02@gmail.com>
2023-02-13 03:21:04 -0300

    solo probando

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -11,6 +11,12 @@ function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+
+  if(edad => 18){
+    return "Allowed";
+  }else{
+    return "Not Allowed";
+  }
 }
   
 function conection(status) {
@@ -44,12 +50,14 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  return numero === 10 || numero === 5;
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  return numero < 50 && numero > 20;
 }
 
 function esEntero(numero) {
